39af7883cc14aa262fe781b4eed3da3f157db011
MADESH122 <madeshwaran9080@gmail.com>
2023-10-01 13:18:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const n10= setTimeout(num10,1000);
+const n9= setTimeout(num9,2000);
+const n8= setTimeout(num8,3000);
+const n7= setTimeout(num7,4000);
+const n6= setTimeout(num6,5000);
+const n5= setTimeout(num5,6000);
+const n4= setTimeout(num4,7000);
+const n3= setTimeout(num3,8000);
+const n2= setTimeout(num2,9000);
+const n1= setTimeout(num1,10000);
+const myTimeout = setTimeout(myGreeting,11000);
+
+
+function num10(){
+    document.getElementById("para").innerText="10"
+  }
+function num9(){
+    document.getElementById("para").innerText="9"
+  }
+  function num8(){
+    document.getElementById("para").innerText="8"
+  }
+  function num7(){
+    document.getElementById("para").innerText="7"
+  }
+  function num6(){
+    document.getElementById("para").innerText="6"
+  }
+  function num5(){
+    document.getElementById("para").innerText="5"
+  }
+  function num4(){
+    document.getElementById("para").innerText="4"
+  }
+  function num3(){
+    document.getElementById("para").innerText="3"
+  }
+  function num2(){
+    document.getElementById("para").innerText="2"
+  }
+  function num1(){
+    document.getElementById("para").innerText="1"
+  }
+  
+              
+  
+
+
+function myGreeting(){
+  document.getElementById("para").innerText="Happy Teachers Day"
+}
